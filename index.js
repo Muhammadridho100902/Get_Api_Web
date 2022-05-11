62d9a84fbfe6bf8ebca6e4e9d353b464359a06b7
unknown <uwater946@gmail.com>
2022-05-11 11:27:01 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', function() {
+    register();
+    getRecipe();
+});
+
+// cara untuk GET Data
+async function getRecipe() {
+    let res = undefined;
+    try {
+        res = await fetch('http://api.fikrihkl.xyz/api/get-recipe', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset-UTF-8'
+            },
+        }).then(data => data.json());
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// cara untuk POST data
+async function register() {
+    var details = {
+        'email': 'ridho@gmail.com',
+        'pass': '123123',
+        'name': 'Ridho',
+        'address': 'idlewood st.2',
+        'phone': '08123'
+    };
+
+    var formBody = [];
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + "-" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    let res = undefined;
+    try {
+        res = await fetch('http://api.fikrihkl.xyz/api/get-recipe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;Charset=UTF-8'
+            },
+            body: formBody
+        }).then(data => data.json());
+        console.log(res);
+    } catch (e) {
+
+    }
+}
